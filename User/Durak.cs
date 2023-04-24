bd4a9958473f6aa1407a0662a270ecb534e30dcb
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-24 21:57:00 +0300

    Добавьте файлы проекта.

---
+++ User/Durak.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Durak
+{
+    class Player
+    {
+        public string name { get; }
+        public List<Card> koloda = new ();
+        public List<SpecialAbilities> sa = new();
+        public bool IsPlaying { get; set; }
+    }
+    class Game
+    {
+        List<Player> p = new();
+        List<Card> koloda = new();
+        List<Card> rebound = new();
+        bool trase;
+
+        public Game(List<Card> buff, List<Player> p_buff, bool transefeble) { }
+        public void give_cards() { }
+        public void mixing() 
+        {
+            
+        }
+    }
+}
